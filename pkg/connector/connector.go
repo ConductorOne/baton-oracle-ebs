197d4d637c8f2b87263fed2cca1308a654809ccf
Martin Albert <martin.albert.187@gmail.com>
2024-04-09 14:58:32 +0200

    feat: prepare connector with users syncing

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -2,43 +2,104 @@ package connector
 
 import (
 	"context"
+	"fmt"
 	"io"
 
+	"github.com/conductorone/baton-oracle-ebs/pkg/ebs"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+
+	ora "github.com/sijms/go-ora/v2"
 )
 
-type Connector struct{}
+type OracleEBS struct {
+	client *ebs.Client
+}
 
 // ResourceSyncers returns a ResourceSyncer for each resource type that should be synced from the upstream service.
-func (d *Connector) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
+func (o *OracleEBS) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
 	return []connectorbuilder.ResourceSyncer{
-		newUserBuilder(),
+		newUserBuilder(o.client),
 	}
 }
 
 // Asset takes an input AssetRef and attempts to fetch it using the connector's authenticated http client
 // It streams a response, always starting with a metadata object, following by chunked payloads for the asset.
-func (d *Connector) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
+func (o *OracleEBS) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
 	return "", nil, nil
 }
 
 // Metadata returns metadata about the connector.
-func (d *Connector) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
+func (o *OracleEBS) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
 	return &v2.ConnectorMetadata{
-		DisplayName: "My Baton Connector",
-		Description: "The template implementation of a baton connector",
+		DisplayName: "OracleEBS",
+		Description: "Connector syncing EBS users and roles to Baton",
 	}, nil
 }
 
 // Validate is called to ensure that the connector is properly configured. It should exercise any API credentials
 // to be sure that they are valid.
-func (d *Connector) Validate(ctx context.Context) (annotations.Annotations, error) {
+func (o *OracleEBS) Validate(ctx context.Context) (annotations.Annotations, error) {
+	err := o.client.Conn.Open()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open connection: %w", err)
+	}
+
+	defer o.client.Conn.Close()
+
+	err = o.client.Conn.Ping(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping connection: %w", err)
+	}
+
 	return nil, nil
 }
 
+const (
+	DEFAULT_PORT    = 1521
+	DEFAULT_SERVER  = "apps.example.com"
+	DEFAULT_SERVICE = "EBSDB"
+)
+
 // New returns a new instance of the connector.
-func New(ctx context.Context) (*Connector, error) {
-	return &Connector{}, nil
+func New(ctx context.Context, cfg ebs.Config) (*OracleEBS, error) {
+	var port int
+	var server, service string
+
+	if cfg.Port == 0 {
+		port = DEFAULT_PORT
+	} else {
+		port = cfg.Port
+	}
+
+	if cfg.Server == "" {
+		server = DEFAULT_SERVER
+	} else {
+		server = cfg.Server
+	}
+
+	if cfg.Service == "" {
+		service = DEFAULT_SERVICE
+	} else {
+		service = cfg.Service
+	}
+
+	connString := ora.BuildUrl(
+		server,
+		port,
+		service,
+		cfg.Username,
+		cfg.Password,
+		nil, // TODO: add trace file for debugging?
+	)
+
+	conn, err := ora.NewConnection(connString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create connection: %w", err)
+	}
+
+	return &OracleEBS{
+		client: ebs.NewEBSClient(conn),
+	}, nil
 }
