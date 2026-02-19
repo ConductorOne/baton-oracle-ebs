40fb8ca3d1be51cfc056a61560db5e8e90455f49
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:07:11 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -37,6 +37,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Oracle EBS connection URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	ConfigurationFields = []field.SchemaField{
